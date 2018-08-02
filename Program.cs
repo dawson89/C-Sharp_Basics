eca79f7fc6f30ced4f34eee1f9dc9c5a953b58b0
Dawson Richey <dawsonrichey@gmail.com>
2018-08-01 23:13:45 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Treehouse.FitnessFrog
+{
+	public class Program
+	{
+		static void Main()
+		{
+			// Prompt user for minutes exercised 
+			Console.Write("Enter how many minutes you exercised: ");
+			string entry = Console.ReadLine();
+
+			// Add minutes exercised to total 
+			// Display total minutes exercised to the screen 
+			Console.WriteLine("You have exercised for " + entry + " minutes.");
+
+			// Repeat until user quits    
+		}
+	}
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehouse.FitnessFrog
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Prompt user for minutes exercised 
+			Console.Write("Enter how many minutes you exercised: ");
+			string entry = Console.ReadLine();
+
+			// Add minutes exercised to total 
+			// Display total minutes exercised to the screen 
+			Console.WriteLine("You have exercised for " + entry + " minutes.");
+			// Repeat until user quits    
+		}
+	}
+}
